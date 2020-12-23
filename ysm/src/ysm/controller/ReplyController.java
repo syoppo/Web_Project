248ee4f11eed5a51d405df98ead064a9264f2e92
syoppo <58536569+syoppo@users.noreply.github.com>
2020-12-24 04:32:00 +0900

    Update ReplyController.java

--- ysm/src/ysm/controller/ReplyController.java
+++ ysm/src/ysm/controller/ReplyController.java
@@ -19,7 +19,6 @@ import org.json.simple.JSONArray;
 @WebServlet("*.rp")
 public class ReplyController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
 	protected void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html; charset=utf-8");
 
