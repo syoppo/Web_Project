a4684607d16ad7b765469e4e066b5939c7ab2bcb
syoppo <58536569+syoppo@users.noreply.github.com>
2020-12-24 04:34:00 +0900

    Update MemberController.java

--- ysm/src/ysm/controller/MemberController.java
+++ ysm/src/ysm/controller/MemberController.java
@@ -21,6 +21,7 @@ import ysm.command.MUpdateCommand;
 
 @WebServlet("*.mb")
 public class MemberController extends HttpServlet{
+	private static final long serialVersionUID = 1L;
 	protected void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 		response.setContentType("text/html; charset=UTF-8");
 		request.setCharacterEncoding("utf-8");
